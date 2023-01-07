a0aa4db9c271593f823f9dbd562a18655a814a70
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-01-07 15:25:46 +0100

    added models and enus id data

---
+++ Dmail.Data/Entites/Models/Spammer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dmail.Data.Entitets.Models
+{
+    public class Spammers
+    {
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public int SpammerId { get; set; }
+        public User Spammer { get; set; } = null!;
+    }
+}
