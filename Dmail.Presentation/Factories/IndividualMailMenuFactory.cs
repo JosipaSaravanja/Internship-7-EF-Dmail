cf60a77f875d1c8dbefb3701ed848398022a79f9
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-01-08 00:36:45 +0100

    added resposne to mails

--- Dmail.Presentation/Factories/IndividualMailMenuFactory.cs
+++ Dmail.Presentation/Factories/IndividualMailMenuFactory.cs
@@ -11,7 +11,6 @@ namespace Dmail.Presentation.Factories
 {
     public class IndividualMailMenuFactory
     {
-
         public static IList<IAction> CreateActions(Mail selected)
         {
             var actions = new List<IAction>
@@ -29,21 +28,14 @@ namespace Dmail.Presentation.Factories
                     RepositoryFactory.Create<MailRepository>(),
                     selected,
                     LogInAction.GetCurrentlyAuthenticatedUser()!),
-                //new ReplayToMail()
-                /*
-            4.Odgovori na mail(ponaša se kao i pošalji novu poštu) ili u slučaju događaja 
-            opcije da se potvrdi ili odbije dolazak na događaj(prilikom čega se automatski 
-            šalje pošiljatelju generična obična pošta o tome što je odabrano sa naznakom da 
-            kao pošiljatelj te pošte stoji korisnik koji je prihvatio ili odbio događaj)
-            */
+                new ReplyToMailAction(
+                    RepositoryFactory.Create<MailRepository>(),
+                    selected,
+                    LogInAction.GetCurrentlyAuthenticatedUser()!),
                 };
 
             actions.SetActionIndexes();
-
             return actions;
-
-
-
         }
     }
 }
