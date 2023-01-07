13e951eb9947ebdd23dc01b9caa9e629cf6bdb07
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-01-07 16:43:51 +0100

    started doing regitratedMenu

---
+++ Dmail.Presentation/Factories/RegistredMenuFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Dmail.Presentation.Actions;
+using Dmail.Presentation.Actions.Main;
+using Dmail.Domain.Repositories;
+using Dmail.Domain.Factories;
+
+namespace Dmail.Presentation.Factories
+{
+    public class RegistredMenuFactory
+    {
+            public static IList<IAction> CreateActions()
+            {
+                var actions = new List<IAction>
+                {
+                new ExitAction(),
+                //Ulazna pošta
+                //izlazna pošta
+                //spam
+                //pošalji movi mail
+                //pošalji movi event
+                //postavke profila
+                //odjava iz profila
+                };
+
+                actions.SetActionIndexes();
+
+                return actions;
+
+            }
+    }
+}
