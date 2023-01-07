5cbcc9d67a3ef285dd8b24a7e3dac64cf0690085
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-01-07 15:39:25 +0100

    added appconfig, context, seeds

---
+++ Dmail.Data/Entites/DmailDBContext.cs
@@ -0,0 +1,95 @@
+﻿using Dmail.Data.Entitets.Models;
+using Dmail.Data.Enums;
+using Dmail.Data.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Dmail.Data.Context
+{
+    public class DmailDBContext : DbContext
+    {
+        public DmailDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User>? Users => Set<User>();
+        public DbSet<Mail>? Mails => Set<Mail>();
+        public DbSet<Recipient>? Recipients => Set<Recipient>();
+        public DbSet<Spammers>? Spammers => Set<Spammers>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().
+                Property(u => u.Email).
+                IsRequired();
+
+            builder.Entity<User>().
+                HasIndex(u => u.Email).
+                IsUnique();
+
+            builder.Entity<User>().
+                Property(u => u.Password).
+                IsRequired();
+
+            builder.Entity<Mail>().
+                Property(m => m.Title).
+                IsRequired();
+
+            builder.Entity<Mail>().
+                Property(m => m.TimeOfCreation).
+                IsRequired().
+                HasDefaultValueSql("timezone('utc', now())");
+
+            builder.Entity<Mail>().
+                Property(m => m.Hide).
+                IsRequired().
+                HasDefaultValue(false);
+
+            builder.Entity<Mail>().
+                Property(m => m.Format).
+                IsRequired();
+
+
+            builder.Entity<Spammers>().
+                HasKey(sf => new { sf.UserId, sf.SpammerId });
+
+            builder.Entity<Spammers>().
+                HasOne(u=>u.User).
+                WithMany(z => z.Spammers).
+                HasForeignKey(sf => sf.UserId);
+
+
+            builder.Entity<Recipient>().
+                HasKey(r => new { r.MailId, r.UserId });
+
+            builder.Entity<Recipient>().
+                Property(r => r.MailStatus).
+                HasDefaultValue(MailStatus.Unread);
+
+            DMailSeeder.Seed(builder);
+
+            base.OnModelCreating(builder);
+        }
+    }
+    public class DmailDBContextFactory : IDesignTimeDbContextFactory<DmailDBContext>
+    {
+        public DmailDBContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("App.config")
+                .Build();
+
+            config.Providers
+                .First()
+                .TryGet("connectionStrings:add:DmailApp:connectionString", out var connectionString);
+
+            var options = new DbContextOptionsBuilder<DmailDBContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+
+            return new DmailDBContext(options);
+        }
+    }
+}
---
+++ Dmail.Data/Seeds/DMailSeeder.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Dmail.Data.Entitets.Models;
+using Dmail.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dmail.Data.Seeds
+{
+	public static class DMailSeeder
+	{
+		public static void Seed(ModelBuilder builder)
+		{
+            //
+
+            builder.Entity<User>()
+                .HasData(new List<User>()
+                {
+                    new User()
+                    {
+                        Id = 1,
+                        Email = "netkoprvi@dmail.hr",
+                        Password = "^uPj226()y",
+                    },
+                    new User()
+                    {
+                        Id = 2,
+                        Email = "netkodrugi@dmail.hr",
+                        Password = "%4$sc9n&aS",
+                    },
+                    new User()
+                    {
+                        Id = 3,
+                        Email = "netkotreci@dmail.hr",
+                        Password = "v^L%9hByBb",
+                    },
+                });
+
+            builder.Entity<Mail>()
+                .HasData(new List<Mail>()
+                {
+                    new Mail()
+                    {
+                        Id = 1,
+                        SenderId = 1,
+                        Title = "prviMail",
+                        TimeOfCreation = DateTime.UtcNow,
+                        Format = Format.Email,
+                        Contents = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                    },
+                    new Mail()
+                    {
+                        Id = 2,
+                        SenderId = 2,
+                        Title = "drugi mail",
+                        TimeOfCreation = DateTime.UtcNow,
+                        Format = Format.Event,
+                        StartOfEvent = DateTime.UtcNow.AddMinutes(1),
+                        LastingOfEvent = TimeSpan.FromMinutes(1),
+                    },
+                });
+
+            builder.Entity<Recipient>()
+                .HasData(new List<Recipient>()
+                {
+                    new Recipient()
+                    {
+                        MailId = 1,
+                        UserId = 2,
+                    },
+                    new Recipient()
+                    {
+                        MailId = 1,
+                        UserId = 3,
+                    },
+                    new Recipient()
+                    {
+                        MailId = 2,
+                        UserId = 1,
+                        EventStatus = EventStatus.NoResponse,
+                    },
+                    new Recipient()
+                    {
+                        MailId = 2,
+                        UserId = 3,
+                        EventStatus = EventStatus.NoResponse,
+                    },
+                });
+
+            builder.Entity<Spammers>()
+                .HasData(new List<Spammers>()
+                {
+                    new Spammers()
+                    {
+                        UserId = 1,
+                        SpammerId = 2,
+                    },
+                    new Spammers()
+                    {
+                        UserId = 1,
+                        SpammerId = 3,
+                    },
+                    new Spammers()
+                    {
+                        UserId = 2,
+                        SpammerId = 3,
+                    },
+                });
+            //
+        }
+	}
+}
